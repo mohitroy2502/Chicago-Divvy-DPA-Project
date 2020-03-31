4c25070fba6444fc4152014de8a2c0f6f9fb18ab
manishtcr7 <61477500+manishtcr7@users.noreply.github.com>
2020-03-31 01:53:39 -0500

    Add files via upload

--- Project.R
+++ Project.R
@@ -1,12 +1,7 @@
 # Importing Dataset
-dataset =read.csv('data_raw.csv', header = TRUE, nrows = 10000)
+dataset =read.csv('data_raw.csv', header = TRUE, nrows = 1000)
 # cleaned dataset
 cleaned =read.csv('data.csv', header = TRUE, nrows = 1000)
-
-# Mohit Dataset
-Q1 <- read.csv('Divvy_Trips_2017_Q1.csv', header = TRUE, nrows = 100)
-Q2 <- read.csv('Divvy_Trips_2017_Q2.csv', header = TRUE, nrows = 100)
-total <- rbind(Q1, Q2)
 
 # Splitting the dataset into the Training set and Test set
 # install.packages('caTools')
@@ -40,23 +35,22 @@ drops <- c("starttime","stoptime", "events", "windchill", "precipitation")
 dataset <- dataset[ , !(names(dataset) %in% drops)]
 
 
-
-
-
 # --------------------------------------------
+# count of rides vs month.
+
 SMonth <- format(as.POSIXct(strptime(dataset$StartDate,"%Y-%m-%d",tz="")) ,format = "%m")
 dataset$StartMonth <- SMonth
 library(ggplot2)
 
-ggplot(dataset, aes(StartMonth)) + geom_bar()
-
-
-
-
-
+ggplot(dataset, aes(StartMonth)) +
+  geom_bar(color = "black", fill = "red") +
+  ggtitle("Count of trips vs Month") +
+  xlab("Month") +
+  ylab("Count of Trips")
 
 
 # Trying to implement Decision Tree.
+
 library(rpart)
 regressor = rpart(formula = to_station_id ~ wind_speed,
                   data = dataset,
@@ -64,11 +58,6 @@ regressor = rpart(formula = to_station_id ~ wind_speed,
 
 
 y_pred = predict(regressor, data.frame(wind_speed = 12.1))
-
-
-
-
-
 
 
 dataset$gender = ifelse(is.na(dataset$gender),
@@ -80,4 +69,48 @@ dataset$gender = ifelse(is.na(dataset$gender),
 
 dat <-dataset[!(dataset$gender==""),]
 
+# **********************************************************************************
 
+# Mohit Datasets
+
+Q1_2017 <- read.csv('Divvy_Trips_2017_Q1.csv', header = TRUE, nrows = 100)
+Q2_2017 <- read.csv('Divvy_Trips_2017_Q2.csv', header = TRUE, nrows = 100)
+Q3_2017 <- read.csv('Divvy_Trips_2017_Q3.csv', header = TRUE, nrows = 100)
+Q4_2017 <- read.csv('Divvy_Trips_2017_Q4.csv', header = TRUE, nrows = 100)
+
+Q1_2018 <- read.csv('Divvy_Trips_2018_Q1.csv', header = TRUE, nrows = 100)
+Q2_2018 <- read.csv('Divvy_Trips_2018_Q2.csv', header = TRUE, nrows = 100)
+Q3_2018 <- read.csv('Divvy_Trips_2018_Q3.csv', header = TRUE, nrows = 100)
+Q4_2018 <- read.csv('Divvy_Trips_2018_Q4.csv', header = TRUE, nrows = 100)
+
+Q1_2019 <- read.csv('Divvy_Trips_2019_Q1.csv', header = TRUE, nrows = 100)
+Q2_2019 <- read.csv('Divvy_Trips_2019_Q2.csv', header = TRUE, nrows = 100)
+Q3_2019 <- read.csv('Divvy_Trips_2019_Q3.csv', header = TRUE, nrows = 100)
+Q4_2019 <- read.csv('Divvy_Trips_2019_Q4.csv', header = TRUE, nrows = 100)
+
+
+# Dataset Q1_2018 column names doesn't match.
+Q1_2018 <- setNames(Q1_2018, c("trip_id", "start_time", "end_time", "bikeid", "tripduration", "from_station_id", "from_station_name", "to_station_id", "to_station_name", "usertype", "gender", "birthyear"))
+
+# Train (2017-2018) and Test (2019)
+TrainDst <- rbind(Q1_2017, Q2_2017, Q3_2017, Q4_2017, Q1_2018, Q2_2018, Q3_2018, Q4_2018)
+TestDst <- rbind(Q1_2019, Q2_2019, Q3_2019, Q4_2019)
+
+# Dock Dataset
+DockDst <- read.csv('Divvy_Bicycle_Stations.csv', header = TRUE)
+
+# Merging Dock dataset
+TrainDst <- merge(TrainDst, DockDst, by.x = "from_station_id", by.y = "ID", all.x = T, all.y = F)
+
+# Differentiating date and time in two different columns for Training Dataset.
+# START TIME
+
+STime <- format(as.POSIXct(strptime(TrainDst$start_time,"%m/%d/%Y %H:%M:%S",tz="")) ,format = "%H:%M:%S")
+STime <- format(as.POSIXct(strptime(TrainDst$start_time,"%Y-%m-%d %H:%M:%S",tz="")) ,format = "%H:%M:%S")
+
+SDate <- format(as.POSIXct(strptime(TrainDst$start_time,"%m/%d/%Y %H:%M:%S",tz="")) ,format = "%m/%d/%Y")
+SDate <- format(as.POSIXct(strptime(TrainDst$start_time,"%Y-%m-%d %H:%M:%S",tz="")) ,format = "%m/%d/%Y")
+
+
+TrainDst$StartTime <- STime
+TrainDst$StartDate <- SDate
