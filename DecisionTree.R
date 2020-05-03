8aee5059dae63392bf62e9a5e5f6d7c4323d71c5
RadhikaBarbole9 <37449551+RadhikaBarbole9@users.noreply.github.com>
2020-05-02 19:23:13 -0500

    Add files via upload

---
+++ DecisionTree.R
@@ -0,0 +1,172 @@
+# Importing required libraries.
+library(lubridate)
+library(stringr)
+# install.packages("dplyr")
+library(dplyr)
+library(data.table)
+library(ggplot2)
+
+# Importing Transformed Datasets.
+
+Q1_2017 <- read.csv('Q1_2017_.csv', header = TRUE)
+Q2_2017 <- read.csv('Q2_2017_.csv', header = TRUE)
+Q3_2017 <- read.csv('Q3_2017_.csv', header = TRUE)
+Q4_2017 <- read.csv('Q4_2017_.csv', header = TRUE)
+
+Q1_2018 <- read.csv('Q1_2018_.csv', header = TRUE)
+Q2_2018 <- read.csv('Q2_2018_.csv', header = TRUE)
+Q3_2018 <- read.csv('Q3_2018_.csv', header = TRUE)
+Q4_2018 <- read.csv('Q4_2018_.csv', header = TRUE)
+
+Q1_2019 <- read.csv('Q1_2019_.csv', header = TRUE)
+Q2_2019 <- read.csv('Q2_2019_.csv', header = TRUE)
+Q3_2019 <- read.csv('Q3_2019_.csv', header = TRUE)
+Q4_2019 <- read.csv('Q4_2019_.csv', header = TRUE)
+
+# Combing Datasets.
+Dst <- rbind(Q1_2017, Q2_2017, Q3_2017, Q4_2017, Q1_2018, Q2_2018, Q3_2018, Q4_2018, Q1_2019, Q2_2019, Q3_2019, Q4_2019)
+
+# Splitting Date and Time in two different Columns.
+m<-str_split_fixed(Dst$MainDate, " ", 2)   
+Dst$MainDate <-m[,1]
+Dst$MainTime <-m[,2]
+
+# Data Cleaning: MainDate present in Varied formats.
+# Hence, made in a single format. (%Y-%m-%d)
+Dst$MainDate <- parse_date_time(Dst$MainDate, orders = c("%m/%d/%Y", "%Y-%m-%d"))
+
+# Droping extra column.
+drops <- c("X")
+Dst <- Dst[ , !(names(Dst) %in% drops)]
+
+# Importing Weather Data.
+WeatherTest <- read.csv('weatherTrain.csv', header = TRUE)
+WeatherTrain <- read.csv('weathertest.csv', header = TRUE)
+
+Weather <- rbind(WeatherTest, WeatherTrain)
+
+Weather$Date <- as.Date(Weather$Date)
+Dst$MainDate <- as.Date(Dst$MainDate)
+
+# Merging weather datasets with Original Dataset.
+Dst <- merge(Dst, Weather, by.x = "MainDate", by.y = "Date", all.x = T, all.y = F)
+
+
+# Dealing with Categorial Variables.
+Dst$bikeid <- as.factor(Dst$bikeid)
+Dst$Weather_Icon <- as.factor(Dst$Weather_Icon)
+
+# Importing Dates vs No of Trips Dataset.
+NoOfTripsDate <- read.csv('DateTrips.csv', header = TRUE)
+
+# Data Cleaning: MainDate present in Varied formats.
+# Hence, made in a single format. (%Y-%m-%d)
+NoOfTripsDate$Date <- parse_date_time(NoOfTripsDate$Date, orders = c("%m/%d/%Y", "%Y-%m-%d"))
+
+NoOfTripsDate$Date <- as.Date(NoOfTripsDate$Date)
+
+# Merging all Datasets.
+Dst <- merge(Dst, NoOfTripsDate, by.x = "MainDate", by.y = "Date", all.x = T, all.y = F)
+
+Dst$TotalTrips <- as.numeric(Dst$TotalTrips)
+
+# Condition when bikes needs Maintenance.
+# Maintenance = 0 (Dosen't need Maintenance)
+# Maintenance = 1 (Needs Maintenance)
+Dst$Maintenance <- 0
+Dst$Maintenance[(Dst$TotalDuration>110000 | Dst$No.Of.Trips>80) & (Dst$Weather_Icon == "clear-day" | Dst$Weather_Icon == "partly-cloudy-day" | Dst$Weather_Icon == "cloudy") & (Dst$TotalTrips < 11000)] <- 1
+
+# Encoding the target feature as factor.
+Dst$Maintenance = factor(Dst$Maintenance, levels = c(0, 1))
+
+# Dividing Training and Testing Dataset.
+# library(caTools)
+# set.seed(1234)
+# split = sample.split(Dst$from_station_name, SplitRatio = 0.8)
+# TrainDst = subset(Dst, split == TRUE)
+# TestDst = subset(Dst, split == FALSE)
+# library('caret')
+# set.seed(1234)
+# partition <- createDataPartition(y = Dst$from_station_name, p = 0.8, list = FALSE)
+# TrainDst <- Dst[partition,]
+# TestDst <- Dst[-partition,]
+# stopifnot(nrow(TrainDst) + nrow(TestDst) == nrow(Dst))
+
+# Dividing the main DataSet into Training and Testing Dataset.
+# 2017-18 as Training Dataset.
+# 2019 as Testing Dataset.
+
+Dst <- as.data.frame(Dst)
+
+Train <- subset(Dst, (Dst$MainDate > "2017-01-01" & Dst$MainDate < "2018-12-31"))
+Test <- subset(Dst, (Dst$MainDate > "2019-01-01" & Dst$MainDate < "2019-12-31"))
+
+# Implementing Decision Tree Classification.
+
+
+# Fitting Decision Tree Classification to the Training set
+# install.packages('rpart')
+library(rpart)
+classifier1 = rpart(formula = Maintenance ~ bikeid + TotalDuration + No.Of.Trips + Weather_Icon + MainDate + TotalTrips,
+                    data = Train)
+
+y_pred1 = predict(classifier1, newdata = Test, type = 'class')
+
+save(classifier1, file = 'C:/Users/manis/Desktop/DT.rda')
+
+
+# Visualize the output.
+
+Test$Maintenance = y_pred1
+
+# library(data.table)
+# Test <- data.table(Test)
+# MainD <- Test[, .(rowCount = .N), by =MainDate]
+# median(MainD$rowCount)
+
+
+Visualize <- function(x){
+  
+  xx=unique(Test[Test$MainDate==x & Test$Maintenance==1,c("bikeid", 'Maintenance', "MainDate", "Weather_Icon")])
+  xx
+}
+
+Visualize1 <- function(x){
+  
+  xx=unique(Test[Test$MainDate==x & Test$Maintenance==0,c("MainDate", "Weather_Icon")])
+  xx
+}
+
+Visualize("2019-01-17")
+Visualize("2019-02-08")
+Visualize("2019-02-18")
+
+xy=unique(Test[Test$MainDate=="2019-02-18" & Test$Maintenance==0,c("bikeid", 'Maintenance', "MainDate", "Weather_Icon")])
+
+
+
+# Making the Confusion Matrix
+library('caret')
+cm1 = confusionMatrix(reference = Test$Maintenance, data = y_pred1)
+cm1
+# Dst$MainDate = as.Date(Dst$MainDate)
+# 
+# regressor = rpart(formula = MainDate ~ bikeid + TotalDuration + No.Of.Trips + Weather_Icon + TotalTrips + Maintenance,
+#                   data = Train,
+#                   control = rpart.control(minsplit = 1))
+# 
+# y_pred = predict(regressor, newdata = Test)
+# 
+# regressor <- lm(formula = MainDate ~ bikeid + TotalDuration + No.Of.Trips + Weather_Icon + Maintenance + TotalTrips,
+#                 data = Train)
+# summary(regressor)
+
+# Class Professor.
+install.packages('rattle')
+install.packages('rpart.plot')
+install.packages('RColorBrewer')
+library(rattle)
+library(rpart.plot)
+library(RColorBrewer)
+
+fancyRpartPlot(classifier1)
---
+++ SupportVectorMachine.R
@@ -0,0 +1,121 @@
+# Importing required libraries.
+library(lubridate)
+library(stringr)
+# install.packages("dplyr")
+library(dplyr)
+library(data.table)
+library(ggplot2)
+
+# Importing Transformed Datasets.
+
+Q1_2017 <- read.csv('Q1_2017_.csv', header = TRUE)
+Q2_2017 <- read.csv('Q2_2017_.csv', header = TRUE)
+Q3_2017 <- read.csv('Q3_2017_.csv', header = TRUE)
+Q4_2017 <- read.csv('Q4_2017_.csv', header = TRUE)
+
+Q1_2018 <- read.csv('Q1_2018_.csv', header = TRUE)
+Q2_2018 <- read.csv('Q2_2018_.csv', header = TRUE)
+Q3_2018 <- read.csv('Q3_2018_.csv', header = TRUE)
+Q4_2018 <- read.csv('Q4_2018_.csv', header = TRUE)
+
+Q1_2019 <- read.csv('Q1_2019_.csv', header = TRUE)
+Q2_2019 <- read.csv('Q2_2019_.csv', header = TRUE)
+Q3_2019 <- read.csv('Q3_2019_.csv', header = TRUE)
+Q4_2019 <- read.csv('Q4_2019_.csv', header = TRUE)
+
+# Combing Datasets.
+Dst <- rbind(Q1_2017, Q2_2017, Q3_2017, Q4_2017, Q1_2018, Q2_2018, Q3_2018, Q4_2018, Q1_2019, Q2_2019, Q3_2019, Q4_2019)
+
+# Splitting Date and Time in two different Columns.
+m<-str_split_fixed(Dst$MainDate, " ", 2)   
+Dst$MainDate <-m[,1]
+Dst$MainTime <-m[,2]
+
+# Data Cleaning: MainDate present in Varied formats.
+# Hence, made in a single format. (%Y-%m-%d)
+Dst$MainDate <- parse_date_time(Dst$MainDate, orders = c("%m/%d/%Y", "%Y-%m-%d"))
+
+# Droping extra column.
+drops <- c("X")
+Dst <- Dst[ , !(names(Dst) %in% drops)]
+
+# Importing Weather Data.
+WeatherTest <- read.csv('weatherTrain.csv', header = TRUE)
+WeatherTrain <- read.csv('weathertest.csv', header = TRUE)
+
+Weather <- rbind(WeatherTest, WeatherTrain)
+
+Weather$Date <- as.Date(Weather$Date)
+Dst$MainDate <- as.Date(Dst$MainDate)
+
+# Merging weather datasets with Original Dataset.
+Dst <- merge(Dst, Weather, by.x = "MainDate", by.y = "Date", all.x = T, all.y = F)
+
+
+# Dealing with Categorial Variables.
+Dst$bikeid <- as.factor(Dst$bikeid)
+Dst$Weather_Icon <- as.factor(Dst$Weather_Icon)
+
+# Importing Dates vs No of Trips Dataset.
+NoOfTripsDate <- read.csv('DateTrips.csv', header = TRUE)
+
+# Data Cleaning: MainDate present in Varied formats.
+# Hence, made in a single format. (%Y-%m-%d)
+NoOfTripsDate$Date <- parse_date_time(NoOfTripsDate$Date, orders = c("%m/%d/%Y", "%Y-%m-%d"))
+
+NoOfTripsDate$Date <- as.Date(NoOfTripsDate$Date)
+
+# Merging all Datasets.
+Dst <- merge(Dst, NoOfTripsDate, by.x = "MainDate", by.y = "Date", all.x = T, all.y = F)
+
+Dst$TotalTrips <- as.numeric(Dst$TotalTrips)
+
+# Condition when bikes needs Maintenance.
+# Maintenance = 0 (Dosen't need Maintenance)
+# Maintenance = 1 (Needs Maintenance)
+Dst$Maintenance <- 0
+Dst$Maintenance[(Dst$TotalDuration>110000 | Dst$No.Of.Trips>80) & (Dst$Weather_Icon == "clear-day" | Dst$Weather_Icon == "partly-cloudy-day" | Dst$Weather_Icon == "cloudy") & (Dst$TotalTrips < 15000)] <- 1
+
+# Encoding the target feature as factor.
+Dst$Maintenance = factor(Dst$Maintenance, levels = c(0, 1))
+
+# Dividing the main DataSet into Training and Testing Dataset.
+# 2017-18 as Training Dataset.
+# 2019 as Testing Dataset.
+Dst <- as.data.frame(Dst)
+
+Train <- subset(Dst, (Dst$MainDate > "2017-01-01" & Dst$MainDate < "2018-12-31"))
+Test <- subset(Dst, (Dst$MainDate > "2019-01-01" & Dst$MainDate < "2019-12-31"))
+
+
+# Implementing Support Vector Machine.
+
+# install.packages('e1071')
+library(e1071)
+
+classifier3 = svm(formula = Maintenance ~ bikeid + TotalDuration + Weather_Icon + TotalTrips, data= Train, type= 'C-classification', kernal= 'linear')
+
+y_pred3 = predict(classifier3, newdata = Test)
+
+cm3 = confusionMatrix(reference = Test$Maintenance, data = y_pred3)
+cm3
+
+# Visualize the output.
+
+Test$Maintenance = y_pred3
+
+Visualize <- function(x){
+  
+  xx=unique(Test[Test$MainDate==x & Test$Maintenance==1,c("bikeid", 'Maintenance', "MainDate", "Weather_Icon")])
+  xx
+}
+
+Visualize1 <- function(x){
+  
+  xx=unique(Test[Test$MainDate==x & Test$Maintenance==0,c("MainDate", "Weather_Icon")])
+  xx
+}
+
+Visualize("2019-01-17")
+Visualize("2019-02-08")
+Visualize("2019-02-18")
